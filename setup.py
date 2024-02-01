9a4e70c449d83329167c924204cd8a2c71a956c3
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-01 19:01:54 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="drift-lens",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     description="DriftLens: an Unsupervised Drift Detection framework",
     long_description=open('README.md').read(),
