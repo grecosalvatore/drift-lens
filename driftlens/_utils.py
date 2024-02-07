d4a2c98eb2561bf078b936f9dd28366973128852
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-07 23:28:30 +0100

    Added docs

--- driftlens/_utils.py
+++ driftlens/_utils.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 
+
 def get_project_root() -> Path:
+    """Returns project root folder."""
     return Path(__file__).parent.parent
 
+
 def clear_complex_numbers(scores):
+    """Clears complex numbers from a list of scores (distances).
+    Args:
+        scores (list): List of scores (distances).
+    Returns:
+        list: List of scores (distances) with complex numbers cleared.
+    """
     return [complex(score).real if type(score) == str else score for score in scores]
 
