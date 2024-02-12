d160b4bdb2a9fb6e9d25e2261a6ddb991426e5bb
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-12 11:02:47 +0100

    Added use_case_1

--- experiments/use_case_1_ag_news_science_drift/drift_detection_accuracy.py
+++ experiments/use_case_1_ag_news_science_drift/drift_detection_accuracy.py
@@ -1,10 +1,15 @@
 import argparse
-from alibi_detect.cd import KSDrift, MMDDrift, LSDDDrift
+from alibi_detect.cd import KSDrift, MMDDrift, LSDDDrift, CVMDrift, ChiSquareDrift
 from experiments.windows_manager.windows_generator import WindowsGenerator
 from driftlens.driftlens import DriftLens
 import os
 import h5py
 from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+import numpy as np
+import json
+import datetime
+import time
 
 
 def parse_args():
@@ -16,11 +21,13 @@ def parse_args():
     parser.add_argument('--batch_n_pc', type=int, default=150)
     parser.add_argument('--per_label_n_pc', type=int, default=75)
     parser.add_argument('--threshold_sensitivity', type=int, default=99)
+    parser.add_argument('--threshold_number_of_estimation_samples', type=int, default=1000)
     parser.add_argument('--train_embedding_filepath', type=str, default=f"{os.getcwd()}/static/saved_embeddings/bert/train_embedding_0_1_2.hdf5")
     parser.add_argument('--test_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/bert/test_embedding_0_1_2.hdf5')
     parser.add_argument('--new_unseen_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/bert/new_unseen_embedding_0_1_2.hdf5')
     parser.add_argument('--drift_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/bert/drift_embedding_3.hdf5')
-    parser.add_argument('--output_dir', type=str, default='outputs/')  # Currently not used
+    parser.add_argument('--output_dir', type=str, default=f"{os.getcwd()}/static/outputs/bert/")
+    parser.add_argument('--save_results', action='store_true')
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--seed', type=int, default=42)
@@ -55,19 +62,17 @@ def main():
     training_label_list = [0, 1, 2]  # Labels used for training - 0: World, 1: Sports, 2: Business
     drift_label_list = [3]  # Labels used for drift simulation - 3: Science/technology
 
-    # Set the file paths for the embeddings
-    #train_embedding_filepath = f"{os.getcwd()}/static/saved_embeddings/{args.model_name}/{args.train_embedding_filename}"
-    #test_embedding_filepath = f"{os.getcwd()}/static/saved_embeddings/{args.model_name}/{args.test_embedding_filename}"
-    #new_unseen_embedding_filepath = f"{os.getcwd()}/static/saved_embeddings/{args.model_name}/{args.new_unseen_embedding_filename}"
-    #drift_embedding_filepath = f"{os.getcwd()}/static/saved_embeddings/{args.model_name}/{args.drift_embedding_filename}"
-
     # Print the current working directory
     print("Current Working Directory:", os.getcwd())
 
-    print(args.model_name)
-    print(args.window_size)
-    print(args.train_embedding_filepath)
+    if args.save_results:
+        if not os.path.exists(args.output_dir):
+            os.makedirs(args.output_dir)
+        ts = time.time()
+        timestamp = str(datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d_%H%M%S'))
+        output_filename = f"drift_detection_accuracy_model_{args.model_name}_win_size_{args.window_size}_n_windows_{args.number_of_windows}_drift_percentage_{args.drift_percentage}_{timestamp}.json"
 
+    # Parse parameters
     window_size = args.window_size
     batch_n_pc = args.batch_n_pc
     per_label_n_pc = args.per_label_n_pc
@@ -79,14 +84,12 @@ def main():
     E_new_unseen, Y_original_new_unseen, Y_predicted_new_unseen = load_embedding(args.new_unseen_embedding_filepath)
     E_drift, Y_original_drift, Y_predicted_drift = load_embedding(args.drift_embedding_filepath)
 
-    print(len(E_train))
-    print(len(E_test))
-    print(len(E_new_unseen))
-    print(len(E_drift))
-
+    # Initialize empty lists of predictions
     ks_preds = []
     mmd_preds = []
     lsdd_preds = []
+    cvm_preds = []
+    chisquare_preds = []
     dl_distances = []
 
     # Initialize the WindowsGenerator - used for creating the windows
@@ -116,20 +119,21 @@ def main():
                                                                                                      batch_n_pc=batch_n_pc,
                                                                                                      per_label_n_pc=per_label_n_pc,
                                                                                                      window_size=window_size,
-                                                                                                     n_samples=10,
+                                                                                                     n_samples=args.threshold_number_of_estimation_samples,
                                                                                                      flag_shuffle=True,
                                                                                                      flag_replacement=True)
 
-    #print(per_batch_distances_sorted, per_label_distances_sorted)
-
-    per_batch_th = max(per_batch_distances_sorted)
+    # Calculate the threshold values
+    l = np.array(per_batch_distances_sorted)
+    l = l[(l > np.quantile(l, 0.01)) & (l < np.quantile(l, 0.99))].tolist()
+    per_batch_th = max(l)
 
     # Initialize drift detectors used for comparison
-    ks_detector = KSDrift(E_train, p_val=.05)
-    mmd_detector = MMDDrift(E_test, p_val=.05, n_permutations=100, backend="pytorch")
-    lsdd_detector = LSDDDrift(E_test, backend='pytorch', p_val=.05)
-
-
+    ks_detector = KSDrift(E_train[:500], p_val=.05)
+    mmd_detector = MMDDrift(E_test[:500], p_val=.05, n_permutations=100, backend="pytorch")
+    lsdd_detector = LSDDDrift(E_test[:500], backend='pytorch', p_val=.05)
+    cvm_detector = CVMDrift(E_train[:500], p_val=.05)
+    chisquare_detector = ChiSquareDrift(E_train[:500], p_val=0.05)
 
     # Generate windows and predict drift
     for i in tqdm(range(n_windows)):
@@ -156,11 +160,16 @@ def main():
         ks_pred = ks_detector.predict(E_windows[0])
         mmd_pred = mmd_detector.predict(E_windows[0])
         lsdd_pred = lsdd_detector.predict(E_windows[0], return_p_val=True, return_distance=True)
+        cvm_pred = cvm_detector.predict(E_windows[0], drift_type='batch', return_p_val=True, return_distance=True)
+        chisquare_pred = chisquare_detector.predict(E_windows[0], drift_type='batch', return_p_val=True, return_distance=True)
 
         # Append the predictions to the lists
         ks_preds.append(ks_pred["data"]["is_drift"])
         mmd_preds.append(mmd_pred["data"]["is_drift"])
         lsdd_preds.append(lsdd_pred["data"]["is_drift"])
+        cvm_preds.append(cvm_pred["data"]["is_drift"])
+        chisquare_preds.append(chisquare_pred["data"]["is_drift"])
+
         dl_distances.append(dl_distance)
 
     if args.drift_percentage > 0:
@@ -175,10 +184,31 @@ def main():
         else:
             dl_preds.append(0)
 
-    print("KS", ks_preds)
-    print("MMD", mmd_preds)
-    print("LSDD", lsdd_preds)
-    print("DriftLens", dl_preds)
+    # Calculate the accuracy of the drift detectors
+    ks_acc = accuracy_score(ground_truth, ks_preds, normalize=True)
+    mmd_acc = accuracy_score(ground_truth, mmd_preds, normalize=True)
+    lsdd_acc = accuracy_score(ground_truth, lsdd_preds, normalize=True)
+    cvm_acc = accuracy_score(ground_truth, cvm_preds, normalize=True)
+    chisquare_acc = accuracy_score(ground_truth, chisquare_preds, normalize=True)
+
+    driftlens_acc = accuracy_score(ground_truth, dl_preds, normalize=True)
+
+
+    print("KS: ", ks_acc)
+    print("MMD: ", mmd_acc)
+    print("LSDD: ", lsdd_acc)
+    print("CVM: ", cvm_acc)
+    print("ChiSquare: ", chisquare_acc)
+
+    print("DriftLens: ", driftlens_acc)
+
+    # Create the output dictionary
+    output_dict = {"params": args,
+                   "accuracy": {"KS": ks_acc, "MMD": mmd_acc, "LSDD": lsdd_acc, "CVM": cvm_acc, "ChiSquare": chisquare_acc, "DriftLens": driftlens_acc}}
+
+    # Save the output dictionary
+    with open(os.path.join(args.output_dir, output_filename), 'w') as fp:
+        json.dump(output_dict, fp)
 
     return
 
--- experiments/use_case_1_bert_drift_detection_accuracy.sh
+++ experiments/use_case_1_bert_drift_detection_accuracy.sh
@@ -5,16 +5,18 @@ cd ..
 python -m experiments.use_case_1_ag_news_science_drift.drift_detection_accuracy \
   --model_name 'bert' \
   --window_size 1000 \
-  --number_of_windows 3 \
+  --number_of_windows 10 \
   --drift_percentage 0 \
   --threshold_sensitivity 99 \
+  --threshold_number_of_estimation_samples 10 \
   --batch_n_p 150 \
   --per_label_n_pc 75 \
   --train_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/train_embedding_0_1_2.hdf5' \
   --test_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/test_embedding_0_1_2.hdf5' \
   --new_unseen_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/new_unseen_embedding_0_1_2.hdf5' \
   --drift_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/drift_embedding_3.hdf5' \
-  --output_dir 'ft_models_b/' \
+  --output_dir 'experiments/use_case_1_ag_news_science_drift/static/outputs/bert/' \
+  --save_results \
   --cuda \
   --verbose \
   --seed 42
