062375621ceb2b8fafa62e4fa67779c1387237d8
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-20 23:12:12 +0100

    Added correlation

--- experiments/use_case_5_2_vgg16_drift_detection_accuracy.sh
+++ experiments/use_case_5_2_vgg16_drift_detection_accuracy.sh
@@ -3,14 +3,16 @@ cd ..
 
 
 python -m experiments.use_case_5_stl_truck_drift.use_case_5_drift_detection_accuracy \
+  --number_of_runs 10 \
   --model_name 'vgg16' \
-  --window_size 500 \
+  --window_size 250 \
   --number_of_windows 100 \
-  --drift_percentage 0 \
+  --drift_percentage 0 5 10 15 20 \
   --threshold_sensitivity 99 \
   --threshold_number_of_estimation_samples 10000 \
   --batch_n_p 150 \
-  --per_label_n_pc 75 \
+  --per_label_n_pc 25 \
+  --n_subsamples_sota 5000 \
   --train_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vgg16/train_embedding.hdf5' \
   --test_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vgg16/test_embedding.hdf5' \
   --new_unseen_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vgg16/new_unseen_embedding.hdf5' \
@@ -20,4 +22,3 @@ python -m experiments.use_case_5_stl_truck_drift.use_case_5_drift_detection_accu
   --cuda \
   --verbose \
   --seed 42
-  #--sota_comparison \
