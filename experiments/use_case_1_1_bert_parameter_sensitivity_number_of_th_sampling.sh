39d975b3935ce2f27dbf6492f37d1f678e8b0b5a
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-26 00:41:15 +0100

    Added parameter sensitivity experiment

---
+++ experiments/use_case_1_1_bert_parameter_sensitivity_number_of_th_sampling.sh
@@ -0,0 +1,23 @@
+# Run the Python script as a module
+cd ..
+
+
+python -m experiments.use_case_1_ag_news_science_drift.use_case_1_parameter_sensitivity_number_of_th_sampling \
+  --number_of_runs 5 \
+  --model_name 'bert' \
+  --window_size 1000 \
+  --number_of_windows 100 \
+  --drift_percentage 0 5 10 15 20 \
+  --threshold_sensitivity 99 \
+  --threshold_number_of_estimation_samples_list 100 1000 5000 10000 25000 \
+  --batch_n_p 150 \
+  --per_label_n_pc 75 \
+  --train_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/train_embedding_0_1_2.hdf5' \
+  --test_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/test_embedding_0_1_2.hdf5' \
+  --new_unseen_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/new_unseen_embedding_0_1_2.hdf5' \
+  --drift_embedding_filepath 'experiments/use_case_1_ag_news_science_drift/static/saved_embeddings/bert/drift_embedding_3.hdf5' \
+  --output_dir 'experiments/use_case_1_ag_news_science_drift/static/outputs/bert/' \
+  --save_results \
+  --cuda \
+  --verbose \
+  --seed 42
---
+++ experiments/use_case_5_1_vit_parameter_sensitivity_number_of_th_sampling.sh
@@ -0,0 +1,23 @@
+# Run the Python script as a module
+cd ..
+
+
+python -m experiments.use_case_5_stl_truck_drift.use_case_5_parameter_sensitivity_number_of_th_sampling \
+  --number_of_runs 5 \
+  --model_name 'vit' \
+  --window_size 1000 \
+  --number_of_windows 100 \
+  --drift_percentage 0 5 10 15 20 \
+  --threshold_sensitivity 99 \
+  --threshold_number_of_estimation_samples_list 100 1000 5000 10000 25000 \
+  --batch_n_p 150 \
+  --per_label_n_pc 25 \
+  --train_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vit/train_embedding.hdf5' \
+  --test_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vit/test_embedding.hdf5' \
+  --new_unseen_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vit/new_unseen_embedding.hdf5' \
+  --drift_embedding_filepath 'experiments/use_case_5_stl_truck_drift/static/saved_embeddings/vit/drift_embedding.hdf5' \
+  --output_dir 'experiments/use_case_5_stl_truck_drift/static/outputs/vit/' \
+  --save_results \
+  --cuda \
+  --verbose \
+  --seed 42
