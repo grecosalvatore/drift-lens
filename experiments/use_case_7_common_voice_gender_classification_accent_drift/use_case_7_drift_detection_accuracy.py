636db626aa1db935d8364634ab60203a967b2307
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-26 16:05:28 +0100

    Added use case 7

--- experiments/use_case_7_common_voice_gender_classification_accent_drift/use_case_7_drift_detection_accuracy.py
+++ experiments/use_case_7_common_voice_gender_classification_accent_drift/use_case_7_drift_detection_accuracy.py
@@ -17,7 +17,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def parse_args():
     parser = argparse.ArgumentParser(description='Train model')
     parser.add_argument('--number_of_runs', type=int, default=1)
-    parser.add_argument('--model_name', type=str, default='vit')
+    parser.add_argument('--model_name', type=str, default='wav2vec')
     parser.add_argument('--window_size', type=int, default=1000)
     parser.add_argument('--number_of_windows', type=int, default=100)
     parser.add_argument('--drift_percentage', type=int, nargs='+', default=[0, 5, 10, 15, 20]),
@@ -26,11 +26,11 @@ def parse_args():
     parser.add_argument('--threshold_sensitivity', type=int, default=99)
     parser.add_argument('--threshold_number_of_estimation_samples', type=int, default=10000)
     parser.add_argument('--n_subsamples_sota', type=int, default=5000)
-    parser.add_argument('--train_embedding_filepath', type=str, default=f"{os.getcwd()}/static/saved_embeddings/vit/train_embedding.hdf5")
-    parser.add_argument('--test_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/vit/test_embedding.hdf5')
-    parser.add_argument('--new_unseen_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/vit/new_unseen_embedding.hdf5')
-    parser.add_argument('--drift_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/vit/drift_embedding.hdf5')
-    parser.add_argument('--output_dir', type=str, default=f"{os.getcwd()}/static/outputs/vit/")
+    parser.add_argument('--train_embedding_filepath', type=str, default=f"{os.getcwd()}/static/saved_embeddings/wav2vec/train_embedding.hdf5")
+    parser.add_argument('--test_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/wav2vec/test_embedding.hdf5')
+    parser.add_argument('--new_unseen_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/wav2vec/new_unseen_embedding.hdf5')
+    parser.add_argument('--drift_embedding_filepath', type=str, default=f'{os.getcwd()}/static/saved_embeddings/wav2vec/drift_embedding.hdf5')
+    parser.add_argument('--output_dir', type=str, default=f"{os.getcwd()}/static/outputs/wav2vec/")
     parser.add_argument('--save_results', action='store_true')
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--verbose', action='store_true')
@@ -98,15 +98,15 @@ def main():
     print("Number of samples threshold: ", args.threshold_number_of_estimation_samples)
     print("Drift percentage: ", args.drift_percentage)
 
-    training_label_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]  # Labels used for training
-    drift_label_list = [9]  # Labels used for drift simulation
+    training_label_list = [0, 1]  # Labels used for training
+    drift_label_list = [2]  # Labels used for drift simulation
 
     if args.save_results:
         if not os.path.exists(args.output_dir):
             os.makedirs(args.output_dir)
         ts = time.time()
         timestamp = str(datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d_%H%M%S'))
-        output_filename = f"drift_detection_accuracy_model_{args.model_name}_conv_win_size_{args.window_size}_n_windows_{args.number_of_windows}_{timestamp}.json"
+        output_filename = f"drift_detection_accuracy_model_{args.model_name}_win_size_{args.window_size}_n_windows_{args.number_of_windows}_{timestamp}.json"
 
     # Parse parameters
     window_size = args.window_size
@@ -114,21 +114,22 @@ def main():
     per_label_n_pc = args.per_label_n_pc
     n_windows = args.number_of_windows
 
-    E_train, Y_original_train, Y_predicted_train = load_embedding(args.train_embedding_filepath)
-    E_test, Y_original_test, Y_predicted_test = load_embedding(args.test_embedding_filepath)
-    E_new_unseen, Y_original_new_unseen, Y_predicted_new_unseen = load_embedding(args.new_unseen_embedding_filepath)
-    E_drift, Y_original_drift, Y_predicted_drift = load_embedding(args.drift_embedding_filepath)
-
-    E_train = E_train.reshape(E_train.shape[0], -1)
-    E_test = E_test.reshape(E_test.shape[0], -1)
-    E_new_unseen = E_new_unseen.reshape(E_new_unseen.shape[0], -1)
-    E_drift = E_drift.reshape(E_drift.shape[0], -1)
+    # Load the embeddings
+    if args.model_name == "vgg16":
+        E_train, Y_original_train, Y_predicted_train = load_embedding(args.train_embedding_filepath, E_name="embedding", Y_original_name="original_label", Y_predicted_name="predicted_label")
+        E_test, Y_original_test, Y_predicted_test = load_embedding(args.test_embedding_filepath, E_name="embedding", Y_original_name="original_label", Y_predicted_name="predicted_label")
+        E_new_unseen, Y_original_new_unseen, Y_predicted_new_unseen = load_embedding(args.new_unseen_embedding_filepath, E_name="embedding", Y_original_name="original_label", Y_predicted_name="predicted_label")
+        E_drift, Y_original_drift, Y_predicted_drift = load_embedding(args.drift_embedding_filepath, E_name="embedding", Y_original_name="original_label", Y_predicted_name="predicted_label")
+    else:
+        E_train, Y_original_train, Y_predicted_train = load_embedding(args.train_embedding_filepath)
+        E_test, Y_original_test, Y_predicted_test = load_embedding(args.test_embedding_filepath)
+        E_new_unseen, Y_original_new_unseen, Y_predicted_new_unseen = load_embedding(args.new_unseen_embedding_filepath)
+        E_drift, Y_original_drift, Y_predicted_drift = load_embedding(args.drift_embedding_filepath)
 
     print("Training samples:", len(E_train))
     print("Test samples:", len(E_test))
     print("New unseen samples:", len(E_new_unseen))
     print("Drift samples:", len(E_drift))
-    print("Embedding shape:", E_train.shape)
 
     ks_acc_dict = {str(p): [] for p in args.drift_percentage}
     mmd_acc_dict = {str(p): [] for p in args.drift_percentage}
