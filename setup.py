d9e779df78106cbc42e8e650d4b4e661110013d7
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-08-12 12:26:10 +0200

    Updated configs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="driftlens",
-    version="0.1.1",
+    version="0.1.5",
     packages=find_packages(),
     description="DriftLens: an Unsupervised Drift Detection framework",
     long_description=open('README.md').read(),
@@ -11,8 +11,13 @@ setup(
     author_email="grecosalvatore94@gmail.com",
     url="https://github.com/grecosalvatore/drift-lens",
     install_requires=[
-        # List your package dependencies here
-         'numpy', 'pandas',
+        "numpy>=1.22.4",
+        "scikit-learn>=0.24.2",
+        "matplotlib~=3.5.1",
+        "pandas>=1.1.3",
+        "scipy>=1.5.0",
+        "tqdm~=4.64.1",
+        "setuptools~=58.0.4",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
