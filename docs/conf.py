82c3e77bb403cb00de92acc8b2cb7258d89f65f2
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-08-13 21:35:06 +0200

    Added docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,41 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'DriftLens'
-copyright = '2024, Salvatore Greco'
-author = 'Salvatore Greco'
-release = '0.1.2'
+copyright = '2024, Your Name'
+author = 'Your Name'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'pydata_sphinx_theme'
 
-html_theme = 'sphinx_rtd_theme'
+# Customize the theme
+html_theme_options = {
+    "github_url": "https://github.com/grecosalvatore/drift-lens",
+    "use_edit_page_button": True,
+    "show_prev_next": False,
+    "primary_color": "#00A1D6",  # Your primary color (from your logo)
+    "secondary_color": "#00405D",  # A darker or complementary color
+    "header_color": "#00A1D6",  # Same as primary or a lighter shade
+}
+
+# Context for the "Edit on GitHub" button
+html_context = {
+    "github_user": "grecosalvatore",  # Your GitHub username
+    "github_repo": "drift-lens",      # Your repository name
+    "github_version": "main",         # The branch you want to link to
+    "doc_path": "docs",               # The path to your documentation source files
+}
+
+# Add your custom CSS file
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
