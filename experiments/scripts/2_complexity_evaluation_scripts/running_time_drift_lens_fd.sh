92ef03f7f711a158b2a255975b1730c8fb3c2b6f
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2025-04-07 11:11:33 +0200

    Added execution times

---
+++ experiments/scripts/2_complexity_evaluation_scripts/running_time_drift_lens_fd.sh
@@ -0,0 +1,22 @@
+# Run the Python script as a module
+cd ../../..
+
+
+python -m experiments.running_time_comparison_fd \
+  --number_of_runs 5 \
+  --training_label_list 0 1 2 \
+  --drift_label_list 3 \
+  --run_driftlens \
+  --run_experiment_reference_window \
+  --run_experiment_window_size \
+  --reference_window_size_range '10000:10000:10000' \
+  --datastream_window_size_range '1000:1000:1000' \
+  --embedding_dimensionality_range '768:768:768' \
+  --fixed_reference_window_size 10000 \
+  --fixed_datastream_window_size 1000 \
+  --fixed_embedding_dimensionality 768 \
+  --batch_n_p 150 \
+  --per_label_n_pc 75 \
+  --output_dir 'experiments/' \
+  --verbose \
+  --seed 42
